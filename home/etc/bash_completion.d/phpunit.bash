ead8f0fb927600f35064d4abc99a3feaea1f006e
Henrique Moody <henriquemoody@gmail.com>
2024-11-27 16:18:27 +0100

    Create script and alias that help dealing with PHP projects

--- home/etc/bash_completion.d/phpunit.bash
+++ home/etc/bash_completion.d/phpunit.bash
@@ -2,17 +2,38 @@
 #
 # Created by Henrique Moody <henriquemoody@gmail.com>
 #
+
+_HELP_CACHE=()
+
+_rgrep() {
+    local content=$(cat <&0)
+    local partial
+    local IFS=$'\n'
+
+    for part in "${@}"; do
+        content=$(grep "${part}" <<< "${content}")
+    done
+
+    echo "${content}"
+
+    [[ ! -z "${content}" ]]
+}
+
 _phpunit()
 {
     COMPREPLY=()
 
+    if [[ -z "${_HELP_CACHE[phpunit]}" ]]; then
+        _HELP_CACHE[phpunit]=$(phpunit --help 2> /dev/null)
+    fi
+
     local cur="${COMP_WORDS[COMP_CWORD]}"
     local prev="${COMP_WORDS[COMP_CWORD-1]}"
-    local opts="--coverage-clover --coverage-crap4j --coverage-html --coverage-php --coverage-text --coverage-xml --log-junit --log-tap --log-json --testdox-html --testdox-text --filter --testsuite --group --exclude-group --list-groups --test-suffix --report-useless-tests --strict-coverage --disallow-test-output --enforce-time-limit --disallow-todo-tests --process-isolation --no-globals-backup --static-backup --colors --columns --columns --stderr --stop-on-error --stop-on-failure --stop-on-risky --stop-on-skipped --stop-on-incomplete -v --verbose --debug --loader --repeat --tap --testdox --printer --bootstrap -c --configuration --no-configuration --include-path -d -h --help --version"
-    local diropts="--coverage-html|--coverage-xml|--include-path"
-    local nocompleteopts="--filter|--testsuite|--group|--exclude-group|--test-suffix|--loader|--repeat|--printer|-d|-h|--help|--version"
+    local opts="--atleast-version --bootstrap --cache-directory --cache-result --check-version --colors --columns --columns --configuration --coverage-clover --coverage-cobertura --coverage-crap4j --coverage-filter --coverage-html --coverage-php --coverage-text= --coverage-xml --covers --debug --default-time-limit --disable-coverage-ignore --disallow-test-output --display-deprecations --display-errors --display-incomplete --display-notices --display-phpunit-deprecations --display-skipped --display-warnings --do-not-cache-result --dont-report-useless-tests --enforce-time-limit --exclude-group --exclude-testsuite --fail-on-deprecation --fail-on-empty-test-suite --fail-on-incomplete --fail-on-notice --fail-on-phpunit-deprecation --fail-on-risky --fail-on-skipped --fail-on-warning --filter --generate-baseline --generate-configuration --globals-backup --group --help --ignore-baseline --include-path --list-groups --list-suites --list-tests --list-tests-xml --log-events-text --log-events-verbose-text --log-junit --log-teamcity --migrate-configuration --no-configuration --no-coverage --no-extensions --no-logging --no-output --no-progress --no-results --only-summary-for-coverage-text --order-by --path-coverage --process-isolation --random-order-seed --reverse-list --show-uncovered-for-coverage-text --static-backup --stderr --stop-on-defect --stop-on-deprecation --stop-on-error --stop-on-failure --stop-on-incomplete --stop-on-notice --stop-on-risky --stop-on-skipped --stop-on-warning --strict-coverage --strict-global-state --teamcity --test-suffix --testdox --testdox-html --testdox-text --testsuite --use-baseline --uses --version --warm-coverage-cache -c -d -h"
+    local diropts="--cache-directory|--coverage-html|--coverage-xml|--coverage-filter"
+    local nocompleteopts="-d|--testsuite|--exclude-testsuite|--group|--exclude-group|--covers|--uses|--filter|--test-suffix|--default-time-limit|--order-by|--random-order-seed|--colors|--columns|--atleast-version|-h|--help|--version|--check-version"
 
-    if [[ ${prev} =~ ${diropts} ]]; then
+    if grep "\\${prev}" <<< "${_HELP_CACHE[phpunit]}" 2> /dev/null | grep -q '<dir>'; then
         COMPREPLY=( $(compgen -d -- ${cur}) )
         return 0
 
